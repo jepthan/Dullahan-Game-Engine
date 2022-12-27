1cab21fccb35c8ea94cf55dd323667549f7f1f12
jepthan <50557700+jepthan@users.noreply.github.com>
2022-12-27 16:39:00 -0600

    Add project files.

---
+++ Dullahan-Game-Engine.cpp
@@ -0,0 +1,12 @@
+﻿// Dullahan-Game-Engine.cpp : Defines the entry point for the application.
+//
+
+#include "Dullahan-Game-Engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
