c58ab35454eec8c1c6bbbb02732f0a81a748233c
jepthan <50557700+jepthan@users.noreply.github.com>
2023-01-01 00:12:43 -0600

    error managment sistem

--- source/Dullahan-Game-Engine.cpp
+++ source/Dullahan-Game-Engine.cpp
@@ -6,6 +6,16 @@
 #include "Window.h"
 #include "Math.h"
 
+
+//Error managment 
+
+void GLAPIENTRY MessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam) {
+	std::cout << "ERROR";
+}
+
+
+
+
 int main()
 {
 	JPH::Window *win = new JPH::Window();
